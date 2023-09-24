29d16d323331ebd2285db10c309261f70b774e0b
dhk767 <dhk767@gmail.com>
2023-09-24 22:26:36 +0900

    [refactor] SelectPointMapViewController 코드 정리

--- Carmunication/Controllers/SessionList/SelectPointMapViewController.swift
+++ Carmunication/Controllers/SessionList/SelectPointMapViewController.swift
@@ -11,25 +11,43 @@ class SelectPointMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        let label = UILabel()
-        label.text = "맵뷰"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        let backButton = UIButton()
-        backButton.setTitle("뒤로", for: .normal)
-        backButton.addTarget(self, action: #selector(backButtonAction), for: .touchUpInside)
-        backButton.setBackgroundImage(.pixel(ofColor: .blue), for: .normal)
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        let vStack = UIStackView(arrangedSubviews: [label, backButton])
-        vStack.translatesAutoresizingMaskIntoConstraints = false
-        vStack.axis = .vertical
-        vStack.alignment = .center
-        vStack.distribution = .fillEqually
+        let vStack = vStackContainer()
         view.addSubview(vStack)
         NSLayoutConstraint.activate([
             vStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             vStack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
+}
+
+// MARK: - Component
+extension SelectPointMapViewController {
+    func vStackContainer() -> UIStackView {
+        let vStack = UIStackView(arrangedSubviews: [titleLabel(), backButton()])
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        vStack.axis = .vertical
+        vStack.alignment = .center
+        vStack.distribution = .fillEqually
+        return vStack
+    }
+    func titleLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "맵뷰"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+    func backButton() -> UIButton {
+        let backButton = UIButton()
+        backButton.setTitle("뒤로", for: .normal)
+        backButton.addTarget(self, action: #selector(backButtonAction), for: .touchUpInside)
+        backButton.setBackgroundImage(.pixel(ofColor: .blue), for: .normal)
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        return backButton
+    }
+}
+
+// MARK: - @objc Method
+extension SelectPointMapViewController {
     @objc func backButtonAction() {
         dismiss(animated: true)
     }
