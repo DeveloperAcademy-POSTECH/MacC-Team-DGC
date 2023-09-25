26a55730ceada20df4e37276ff07f9cb4082648d
Taerogrammer <hyung54321@naver.com>
2023-09-26 00:25:56 +0900

    [design] #6 - 세션 시작 뷰 그룹(스토리 형식) 디자인 생성

---
+++ Carmunication/Controllers/SessionStart/GroupCollectionViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  GroupCollectionViewCell.swift
+//  Carmunication
+//
+//  Created by 김태형 on 2023/09/25.
+//
+
+import UIKit
+
+class GroupCollectionViewCell: UICollectionViewCell {
+    
+    // 데이터들(groupImage, groupNameLabel)이 fileprivate이기 때문에 위 식을 통해 뷰 컨트롤러에 데이터 전달
+    var groupData: GroupData? {
+        didSet {
+            guard let groupData = groupData else { return }
+            groupImage.image = groupData.image
+            groupNameLabel.text = groupData.groupName
+            
+        }
+    }
+    
+    fileprivate let groupImage: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        
+        return iv
+    }()
+    
+    fileprivate let groupNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .black // 원하는 텍스트 색상으로 설정
+        label.font = UIFont.systemFont(ofSize: 14) // 원하는 폰트 및 크기로 설정
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setGroupCollectionViewCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+extension GroupCollectionViewCell {
+    
+    private func setGroupCollectionViewCell() {
+        contentView.addSubview(groupImage)
+        contentView.addSubview(groupNameLabel)
+        
+        NSLayoutConstraint.activate([
+            groupImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            groupImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            groupImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            groupNameLabel.topAnchor.constraint(equalTo: groupImage.bottomAnchor, constant: 12), // groupImage 아래에 배치하고 위쪽 간격 조정
+            groupNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            groupNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            groupNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            
+        ])
+    }
+    
+}
--- Carmunication/Controllers/SessionStartViewController.swift
+++ Carmunication/Controllers/SessionStartViewController.swift
@@ -7,10 +7,78 @@
 
 import UIKit
 
-final class SessionStartViewController: UIViewController {
+struct GroupData {
+    var image: UIImage
+    var groupName: String
+}
 
+final class SessionStartViewController: UIViewController {
+    
+    let groupData = [
+        GroupData(image: UIImage(systemName: "heart")!, groupName: "group1"),
+        GroupData(image: UIImage(systemName: "circle")!, groupName: "group2"),
+        GroupData(image: UIImage(systemName: "heart.fill")!, groupName: "group3"),
+        GroupData(image: UIImage(systemName: "circle.fill")!, groupName: "group4"),
+        GroupData(image: UIImage(systemName: "square")!, groupName: "group5"),
+        GroupData(image: UIImage(systemName: "heart")!, groupName: "group6")
+    ]
+
+    fileprivate let groupCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal    // 좌우로 스크롤
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20) // 여백 조정
+
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(GroupCollectionViewCell.self, forCellWithReuseIdentifier: "groupCell")
+        
+        cv.showsHorizontalScrollIndicator = false   // 스크롤바 숨기기
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setCollectionView()
+    }
+}
+
+extension SessionStartViewController {
+    
+    private func setCollectionView() {
+        view.addSubview(groupCollectionView)
+        groupCollectionView.backgroundColor = .white
+        
+        NSLayoutConstraint.activate([
+            groupCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            groupCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            groupCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            groupCollectionView.heightAnchor.constraint(equalTo: groupCollectionView.widthAnchor, multiplier: 0.5)
+        ])
+        
+        groupCollectionView.delegate = self
+        groupCollectionView.dataSource = self
+    }
+    
+    
+}
+
+extension SessionStartViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return groupData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 80, height: 106)    // 변경 필요
+    }
+}
+
+extension SessionStartViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = groupCollectionView.dequeueReusableCell(withReuseIdentifier: "groupCell", for: indexPath) as! GroupCollectionViewCell
+        cell.groupData = self.groupData[indexPath.row]
+        
+        return  cell
     }
 }
