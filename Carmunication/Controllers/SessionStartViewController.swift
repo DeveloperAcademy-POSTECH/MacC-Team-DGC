773835ab6f90553c0849a3f08e701867bd36eb82
Taerogrammer <hyung54321@naver.com>
2023-09-27 15:10:21 +0900

    [chore] #6 - 그룹 데이터 구조 변경

--- Carmunication/Controllers/SessionStartViewController.swift
+++ Carmunication/Controllers/SessionStartViewController.swift
@@ -7,19 +7,60 @@
 
 import UIKit
 
+// 이름은 데이터 구축되면 바꿀 예정입니다.
 struct GroupData {
     var image: UIImage
     var groupName: String
+    var start: String   // 출발지
+    var end: String     // 도착지
+    var startTime: String   // 출발 시간
+    var endTime: String     // 도착 시간
+    var date: String        // 타는 요일
+    var total: Int          // 총 인원
 }
 
 final class SessionStartViewController: UIViewController {
     let groupData = [
-        GroupData(image: UIImage(systemName: "heart")!, groupName: "group1"),
-        GroupData(image: UIImage(systemName: "circle")!, groupName: "group2"),
-        GroupData(image: UIImage(systemName: "heart.fill")!, groupName: "group3"),
-        GroupData(image: UIImage(systemName: "circle.fill")!, groupName: "group4"),
-        GroupData(image: UIImage(systemName: "square")!, groupName: "group5"),
-        GroupData(image: UIImage(systemName: "heart")!, groupName: "group6")
+        GroupData(image: UIImage(systemName: "heart")!, 
+                  groupName: "group1",
+                  start: "양덕",
+                  end: "C5",
+                  startTime: "08:30",
+                  endTime: "9:00",
+                  date: "주중(월 - 금)",
+                  total: 4),
+        GroupData(image: UIImage(systemName: "circle")!, 
+                  groupName: "group2",
+                  start: "포항",
+                  end: "부산",
+                  startTime: "08:30",
+                  endTime: "9:00",
+                  date: "주중(월 - 금)", 
+                  total: 4),
+        GroupData(image: UIImage(systemName: "heart.fill")!, 
+                  groupName: "group3",
+                  start: "인천",
+                  end: "서울",
+                  startTime: "08:30",
+                  endTime: "9:00",
+                  date: "주중(월 - 금)",
+                  total: 4),
+        GroupData(image: UIImage(systemName: "circle.fill")!, 
+                  groupName: "group4",
+                  start: "부평",
+                  end: "일산",
+                  startTime: "08:30",
+                  endTime: "9:00",
+                  date: "주중(월 - 금)",
+                  total: 4),
+        GroupData(image: UIImage(systemName: "square")!, 
+                  groupName: "group5",
+                  start: "서울",
+                  end: "포항",
+                  startTime: "08:30",
+                  endTime: "9:00",
+                  date: "주중(월 - 금)",
+                  total: 4),
     ]
     let journeyTogetherButton: UIButton = {
         let btn = UIButton()
