1170e76f6e23e7d9561b820ec1a3631a40b46dc7
Taerogrammer <hyung54321@naver.com>
2023-09-27 15:00:11 +0900

    [chore] #6 - SwiftLint 적용

--- Carmunication/Controllers/SessionStartViewController.swift
+++ Carmunication/Controllers/SessionStartViewController.swift
@@ -13,7 +13,6 @@ struct GroupData {
 }
 
 final class SessionStartViewController: UIViewController {
-    
     let groupData = [
         GroupData(image: UIImage(systemName: "heart")!, groupName: "group1"),
         GroupData(image: UIImage(systemName: "circle")!, groupName: "group2"),
@@ -22,7 +21,6 @@ final class SessionStartViewController: UIViewController {
         GroupData(image: UIImage(systemName: "square")!, groupName: "group5"),
         GroupData(image: UIImage(systemName: "heart")!, groupName: "group6")
     ]
-    
     let journeyTogetherButton: UIButton = {
         let btn = UIButton()
         btn.setTitle("여정 함께하기", for: .normal)
@@ -39,14 +37,12 @@ final class SessionStartViewController: UIViewController {
         layout.scrollDirection = .horizontal    // 좌우로 스크롤
         layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20) // 여백 조정
 
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.register(GroupCollectionViewCell.self, forCellWithReuseIdentifier: "groupCell")
-        
-        cv.showsHorizontalScrollIndicator = false   // 스크롤바 숨기기
-        return cv
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(GroupCollectionViewCell.self, forCellWithReuseIdentifier: "groupCell")
+        collectionView.showsHorizontalScrollIndicator = false   // 스크롤바 숨기기
+        return collectionView
     }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -56,57 +52,50 @@ final class SessionStartViewController: UIViewController {
 }
 
 extension SessionStartViewController {
-    
     private func setCollectionView() {
         view.addSubview(groupCollectionView)
         groupCollectionView.backgroundColor = .white
-        
         NSLayoutConstraint.activate([
             groupCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
             groupCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             groupCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             groupCollectionView.heightAnchor.constraint(equalTo: groupCollectionView.widthAnchor, multiplier: 0.5)
         ])
-        
         groupCollectionView.delegate = self
         groupCollectionView.dataSource = self
     }
-    
     private func setJourneyTogetherButton() {
         view.addSubview(journeyTogetherButton)
-        
         NSLayoutConstraint.activate([
             journeyTogetherButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -120),
             journeyTogetherButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             journeyTogetherButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             journeyTogetherButton.heightAnchor.constraint(equalToConstant: 120)  // 버튼의 높이 조절
         ])
-        
         journeyTogetherButton.addTarget(self, action: #selector(goToMapView), for: .touchUpInside)
     }
-    
     @objc private func goToMapView() {
         print("Touch")
     }
-    
 }
 
 extension SessionStartViewController: UICollectionViewDelegateFlowLayout {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return groupData.count
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+func collectionView(_ collectionView: UICollectionView,
+                    layout collectionViewLayout: UICollectionViewLayout,
+                    sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 80, height: 106)    // 변경 필요
     }
 }
 
 extension SessionStartViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = groupCollectionView.dequeueReusableCell(withReuseIdentifier: "groupCell", for: indexPath) as! GroupCollectionViewCell
-        cell.groupData = self.groupData[indexPath.row]
-        
-        return  cell
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = groupCollectionView.dequeueReusableCell(withReuseIdentifier: "groupCell", for: indexPath)
+                    as? GroupCollectionViewCell
+        cell?.groupData = self.groupData[indexPath.row]
+        return  cell!
     }
 }
