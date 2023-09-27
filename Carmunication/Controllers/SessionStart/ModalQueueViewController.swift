b44527745031b9554e48a80a2ff4ce618bda211c
Taerogrammer <hyung54321@naver.com>
2023-09-27 16:29:50 +0900

    [feat] #6 - 모달창 버튼 생성

--- Carmunication/Controllers/SessionStart/ModalQueueViewController.swift
+++ Carmunication/Controllers/SessionStart/ModalQueueViewController.swift
@@ -11,10 +11,21 @@ class ModalQueueViewController: UIViewController {
     override var sheetPresentationController: UISheetPresentationController? {
         presentationController as? UISheetPresentationController
     }
+    var startButton: UIButton = {
+        let startBtn = UIButton()
+        startBtn.translatesAutoresizingMaskIntoConstraints = false
+        startBtn.setTitle("바로 시작", for: .normal)
+        startBtn.backgroundColor = .systemBlue
+        startBtn.setTitleColor(.white, for: .normal)
+        startBtn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        startBtn.layer.cornerRadius = 8
+        return startBtn
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setSheetPresentationController()
+        setStartButton()
     }
 }
 extension ModalQueueViewController {
@@ -22,6 +33,17 @@ extension ModalQueueViewController {
         sheetPresentationController?.delegate = self
         sheetPresentationController?.prefersGrabberVisible = true
         sheetPresentationController?.detents = [.medium()]
+    }
+    private func setStartButton() {
+        view.addSubview(startButton)
+        NSLayoutConstraint.activate([
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        startButton.addTarget(self, action: #selector(goToMapView), for: .touchUpInside)
+    }
+    @objc private func goToMapView() {
+        print("asd")
     }
 }
 extension ModalQueueViewController: UISheetPresentationControllerDelegate {
