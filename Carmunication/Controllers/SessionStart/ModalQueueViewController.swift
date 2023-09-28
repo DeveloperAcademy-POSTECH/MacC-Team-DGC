14c886681537a516eaba35c0559bac00f56e89a6
Taerogrammer <hyung54321@naver.com>
2023-09-28 09:55:47 +0900

    [chore] #6 - ModalQueueViewController에 final 추가

--- Carmunication/Controllers/SessionStart/ModalQueueViewController.swift
+++ Carmunication/Controllers/SessionStart/ModalQueueViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ModalQueueViewController: UIViewController {
+final class ModalQueueViewController: UIViewController {
     override var sheetPresentationController: UISheetPresentationController? {
         presentationController as? UISheetPresentationController
     }
