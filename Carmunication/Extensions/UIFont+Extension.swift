cad120e0c8e70c963d1819a10ea1097818cbb070
dhk767 <dhk767@gmail.com>
2023-10-01 23:14:36 +0900

    [Feat] #17 - Font : CarmuFont Enum 코드 정리 완료

--- Carmunication/Extensions/UIFont+Extension.swift
+++ Carmunication/Extensions/UIFont+Extension.swift
@@ -15,83 +15,14 @@ extension UIFont {
      형식으로 사용
      */
     enum CarmuFont {
-        case subhead1
-        case subhead2
-        case subhead3
-        case headline1
-        case headline2
-        case display1
-        case display2
-        case display3
-        case body1
-        case body1Long
-        case body2
-        case body2Long
-        case body3
-        case body3Long
+        case subhead1, subhead2, subhead3
+        case headline1, headline2
+        case display1, display2, display3
+        case body1, body1Long
+        case body2, body2Long
+        case body3, body3Long
         func applyFont(to label: UILabel) {
-            let fontName: String // SF Pro 폰트의 이름
-            var fontSize: CGFloat
-            var lineHeight: CGFloat // Line height 조정
-            var letterSpacing: CGFloat = -0.6// Letter spacing 조정
-            switch self {
-            case .subhead1:
-                fontName = "SFPro-Bold"
-                fontSize = 12
-                lineHeight = 12
-            case .subhead2:
-                fontName = "SFPro-Bold"
-                fontSize = 14
-                lineHeight = 14
-            case .subhead3:
-                fontName = "SFPro-Bold"
-                fontSize = 16
-                lineHeight = 22
-            case .headline1:
-                fontName = "SFPro-Bold"
-                fontSize = 20
-                lineHeight = 28
-            case .headline2:
-                fontName = "SFPro-Bold"
-                fontSize = 24
-                lineHeight = 34
-            case .display1:
-                fontName = "SFPro-Bold"
-                fontSize = 28
-                lineHeight = 38
-            case .display2:
-                fontName = "SFPro-Bold"
-                fontSize = 32
-                lineHeight = 42
-            case .display3:
-                fontName = "SFPro-Bold"
-                fontSize = 36
-                lineHeight = 46
-            case .body1:
-                fontName = "SFPro-Regular"
-                fontSize = 12
-                lineHeight = 12
-            case .body1Long:
-                fontName = "SFPro-Regular"
-                fontSize = 12
-                lineHeight = 18
-            case .body2:
-                fontName = "SFPro-Regular"
-                fontSize = 14
-                lineHeight = 14
-            case .body2Long:
-                fontName = "SFPro-Regular"
-                fontSize = 14
-                lineHeight = 18
-            case .body3:
-                fontName = "SFPro-Regular"
-                fontSize = 16
-                lineHeight = 22
-            case .body3Long:
-                fontName = "SFPro-Regular"
-                fontSize = 16
-                lineHeight = 28
-            }
+            let letterSpacing = -0.6// Letter spacing 조정
             guard let customFont = UIFont(name: fontName, size: fontSize) else {
                 // 폰트를 찾을 수 없는 경우 기본 시스템 폰트 사용
                 return
@@ -104,6 +35,56 @@ extension UIFont {
                 .paragraphStyle: paragraphStyle
             ]
             label.attributedText = NSAttributedString(string: label.text ?? "", attributes: attributes)
+        }
+        private var fontName: String {
+            switch self {
+            case .subhead1, .subhead2, .subhead3, .headline1, .headline2, .display1, .display2, .display3:
+                return "SFPro-Bold"
+            case .body1, .body1Long, .body2, .body2Long, .body3, .body3Long:
+                return "SFPro-Regular"
+            }
+        }
+        private var fontSize: CGFloat {
+            switch self {
+            case .subhead1, .body1, .body1Long:
+                return 12
+            case .subhead2, .body2, .body2Long:
+                return 14
+            case .subhead3, .body3, .body3Long:
+                return 16
+            case .headline1:
+                return 20
+            case .headline2:
+                return 24
+            case .display1:
+                return 28
+            case .display2:
+                return 32
+            case .display3:
+                return 36
+            }
+        }
+        private var lineHeight: CGFloat {
+            switch self {
+            case .subhead1, .body1:
+                return 12
+            case .subhead2, .body2:
+                return 14
+            case .body1Long, .body2Long:
+                return 18
+            case .subhead3, .body3:
+                return 22
+            case .headline1, .body3Long:
+                return 28
+            case .headline2:
+                return 34
+            case .display1:
+                return 38
+            case .display2:
+                return 42
+            case .display3:
+                return 46
+            }
         }
     }
 }
