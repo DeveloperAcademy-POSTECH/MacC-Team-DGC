d255dd94081d83692a275ff2cfb47309075ff4ef
kybeen <rei980627@gmail.com>
2023-10-03 01:13:16 +0900

    [refactor] #14 - root 뷰 컨트롤러 LoginViewController로 수정

--- Carmunication/SceneDelegate.swift
+++ Carmunication/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MyPageViewController()
+        window.rootViewController = LoginViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
