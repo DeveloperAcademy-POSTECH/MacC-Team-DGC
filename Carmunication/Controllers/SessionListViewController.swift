bdf7b85bdccae7a93779a96010dc95b1f6b3e65d
dhk767 <dhk767@gmail.com>
2023-10-03 16:15:03 +0900

    [Feat] #19 - UIImage Extension 코드 분리, 새로운 파일로 이동

--- Carmunication/Controllers/SessionListViewController.swift
+++ Carmunication/Controllers/SessionListViewController.swift
@@ -217,26 +217,6 @@ extension SessionListViewController {
     }
 }
 
-extension UIImage {
-
-    public static func pixel(ofColor color: UIColor) -> UIImage {
-        let pixel = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
-        UIGraphicsBeginImageContext(pixel.size)
-
-        defer {
-            UIGraphicsEndImageContext()
-        }
-
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return UIImage()
-        }
-        context.setFillColor(color.cgColor)
-        context.fill(pixel)
-
-        return UIGraphicsGetImageFromCurrentImageContext() ?? UIImage()
-    }
-}
-
 // MARK: - Preview canvas 세팅
 import SwiftUI
 
