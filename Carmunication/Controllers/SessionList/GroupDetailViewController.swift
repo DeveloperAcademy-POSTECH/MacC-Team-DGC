749d1f43f003cec95de03ab4bde5c640333db2fb
dhk767 <dhk767@gmail.com>
2023-10-05 23:38:25 +0900

    [Feat] #27 - 디테일뷰 셀 레이아웃 구성 완료

--- Carmunication/Controllers/SessionList/GroupDetailViewController.swift
+++ Carmunication/Controllers/SessionList/GroupDetailViewController.swift
@@ -4,8 +4,10 @@
 //
 //  Created by 김동현 on 2023/09/24.
 //
-import SnapKit
+
 import UIKit
+
+import SnapKit
 
 final class GroupDetailViewController: UIViewController {
 
@@ -67,189 +69,52 @@ extension GroupDetailViewController: UITableViewDataSource, UITableViewDelegate 
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        // cellData가 비어있지 않을 때 기존의 CustomListTableViewCell을 반환
-//        if let cell = tableView.dequeueReusableCell(
-//            withIdentifier: "cell",
-//            for: indexPath
-//        ) as? GroupDetailTableViewCell {
-//
-//            cell.cellCount = CGFloat((selectedGroup?.stopoverPoint.count ?? 0) + 2)
-//            cell.index = CGFloat(indexPath.row)
-//            print("delegate 내부 cell.cellCount : ", cell.cellCount)
-//            print("delegate 내부 cell.index : ", cell.index)
-//            return cell
-//        }
         let cell = GroupDetailTableViewCell(
             index: CGFloat(indexPath.row),
             cellCount: CGFloat((selectedGroup?.stopoverPoint.count ?? 0) + 2)
         )
-//        cell.cellCount = CGFloat((selectedGroup?.stopoverPoint.count ?? 0) + 2)
-//        cell.index = CGFloat(indexPath.row)
-        return cell
+        // 셀 데이터 입력 부분. 리팩토링 필요
+        cell.crewCount = 3
+        cell.pointNameLabel.text = {
+            switch indexPath.row {
+            case 0:
+                return "출발지"
+            case (selectedGroup?.stopoverPoint.count ?? 0) + 1:
+                return "도착지"
+            default:
+                return "경유지 \(indexPath.row)"
+            }
+        }()
+        cell.pointName.text = {
+            switch indexPath.row {
+            case 0:
+                return "출발지의 대표 명칭"
+            case (selectedGroup?.stopoverPoint.count ?? 0) + 1:
+                return "도착지의 대표 명칭"
+            default:
+                return "경유지 \(indexPath.row)의 대표명칭"
+            }
+        }()
+        cell.timeLabel.text = "00:00 AM"
+        cell.detailAddress.text = {
+            switch indexPath.row {
+            case 0:
+                return selectedGroup?.startPointDetailAddress
+            case (selectedGroup?.stopoverPoint.count ?? 0) + 1:
+                return selectedGroup?.endPointDetailAddress
+            default:
+                return selectedGroup?.stopoverPoint["경유지\(indexPath.row)"]
+            }
+        }()
+        cell.boardingCrewLabel.text = "탑승 크루"
 
-//        return UITableViewCell()
+        return cell
     }
 
     func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
         return false
     }
 
-}
-
-class GroupDetailTableViewCell: UITableViewCell {
-
-    init(index:CGFloat, cellCount:CGFloat, style: UITableViewCell.CellStyle = .default, reuseIdentifier: String? = nil) {
-        self.index = index
-        self.cellCount = cellCount
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI()
-        setupConstraints()
-    }
-    var index: CGFloat
-    var cellCount: CGFloat
-    // CustomListTableViewCell의 상단 레이블, 이미지
-    let stopPointImage = UIImageView(image: UIImage(named: "StopPoint"))
-    lazy var gradiantLine = GradientLineView(index: index, cellCount: cellCount)
-    let groupName = UILabel()
-    let chevronLabel = UIImageView(image: UIImage(systemName: "chevron.right"))
-
-    // CustomListTableViewCell의 왼쪽 하단 레이블
-    let startPointLabel = UILabel()
-    let directionLabel = UILabel()
-    let endPointLabel = UILabel()
-    let startTimeTextLabel = UILabel()
-    let startTimeLabel = UILabel()
-
-    // CustomListTableViewCell의 오른쪽 하단 이미지 스택
-    let elipseImage = UIImageView(image: UIImage(named: "elipse"))
-    let crewImage = UIStackView()
-    var crewCount: Int = 0 {
-        didSet {
-            updateCrewImages()
-        }
-    }
-
-    // MARK: - Override Function
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        setupUI()
-//        setupConstraints()
-//    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-//        super.init(coder: aDecoder)
-//        setupUI()
-//        setupConstraints()
-    }
-
-    // MARK: - UI Setup function
-    /**
-     스택에 이미지를 추가하는 메서드
-     */
-    private func updateCrewImages() {
-        // 스택뷰의 모든 서브뷰를 제거하여 이미지를 다시 추가
-        for subview in crewImage.arrangedSubviews {
-            crewImage.removeArrangedSubview(subview)
-            subview.removeFromSuperview()
-        }
-
-        // TODO: - 3인 이상일 경우 외 2명 레이블 추가 표시
-        // crewCount 값에 따라 이미지를 반복해서 추가
-        for index in 0..<crewCount {
-            let imageView = UIImageView(image: UIImage(named: "CrewImageDefalut")) // 사용자 이미지로 이름 바꿔 사용
-            imageView.contentMode = .scaleAspectFit
-            imageView.clipsToBounds = true
-            crewImage.addArrangedSubview(imageView)
-
-            if index > 2 { break }
-        }
-    }
-
-    private func setupUI() {
-
-        stopPointImage.contentMode = .scaleAspectFill
-        gradiantLine.draw(CGRect(x: 0, y: 0, width: 2, height: 150))
-        gradiantLine.backgroundColor = .gray
-        // Crew Image 스택 관련 설정
-        crewImage.axis = .horizontal
-        crewImage.alignment = .leading
-        crewImage.distribution = .fillEqually
-
-        // Font, TextColor 설정
-        groupName.font = UIFont.carmuFont.subhead3
-        groupName.textColor = UIColor.semantic.textPrimary
-        chevronLabel.tintColor = UIColor.semantic.accPrimary
-        startPointLabel.font = UIFont.carmuFont.subhead2
-        startPointLabel.textColor = UIColor.semantic.accPrimary
-        endPointLabel.font = UIFont.carmuFont.subhead2
-        endPointLabel.textColor = UIColor.semantic.accPrimary
-        startTimeTextLabel.textColor = UIColor.theme.darkblue6
-        startTimeTextLabel.font = UIFont.carmuFont.body2
-        startTimeLabel.font = UIFont.carmuFont.subhead2
-        startTimeLabel.textColor = UIColor.semantic.accPrimary
-        directionLabel.font = UIFont.carmuFont.body2
-        directionLabel.textColor = UIColor.theme.darkblue6
-
-        contentView.addSubview(gradiantLine)
-        contentView.addSubview(stopPointImage)
-        contentView.addSubview(groupName)
-        contentView.addSubview(chevronLabel)
-        contentView.addSubview(startPointLabel)
-        contentView.addSubview(directionLabel)
-        contentView.addSubview(endPointLabel)
-        contentView.addSubview(startTimeTextLabel)
-        contentView.addSubview(startTimeLabel)
-        contentView.addSubview(crewImage)
-    }
-
-    private func setupConstraints() {
-        let padding: CGFloat = 20
-
-        gradiantLine.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(8)
-        }
-
-        stopPointImage.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(1)
-            make.width.lessThanOrEqualTo(16)
-            make.height.lessThanOrEqualTo(16)
-        }
-
-        chevronLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(padding)
-            make.trailing.equalToSuperview().inset(padding)
-        }
-
-        startTimeTextLabel.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(padding)
-            make.leading.equalToSuperview().inset(padding)
-        }
-
-        startPointLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(padding)
-            make.bottom.equalTo(startTimeTextLabel.snp.top).offset(-4)
-            make.width.lessThanOrEqualTo(100)
-        }
-
-        directionLabel.snp.makeConstraints { make in
-            make.leading.greaterThanOrEqualTo(startPointLabel.snp.trailing).offset(4)
-            make.centerY.equalTo(startPointLabel.snp.centerY)
-        }
-
-        endPointLabel.snp.makeConstraints { make in
-            make.leading.equalTo(directionLabel.snp.trailing).offset(4)
-            make.centerY.equalTo(startPointLabel.snp.centerY)
-            make.width.lessThanOrEqualTo(100)
-        }
-
-        crewImage.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().inset(20)
-            make.bottom.equalToSuperview().inset(20)
-            make.height.equalTo(30)
-            make.width.equalTo(84)
-        }
-    }
 }
 
 // MARK: - Component & Stacks
