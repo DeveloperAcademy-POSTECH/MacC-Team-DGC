6d54710c95254e155d405390070d631e1002b411
kybeen <rei980627@gmail.com>
2023-10-09 01:49:34 +0900

    [feat] #28 - 이메일로 문의하기 구현

--- Carmunication/Controllers/MyPage/InquiryViewController.swift
+++ Carmunication/Controllers/MyPage/InquiryViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 김영빈 on 2023/10/01.
 //
 
+import MessageUI
 import UIKit
 
 final class InquiryViewController: UIViewController {
@@ -26,8 +27,50 @@ final class InquiryViewController: UIViewController {
         inquiryView.inquiryTableView.dataSource = self
         inquiryView.inquiryTableView.delegate = self
     }
+
+    // MARK: - 이메일 작성 화면 띄우기
+    private func showEmailInquiryView() {
+        // 이메일을 사용 가능한 지 체크 (디바이스 내 Mail앱을 이용할 수 있는지)
+        if MFMailComposeViewController.canSendMail() {
+            let compseVC = MFMailComposeViewController()
+            compseVC.mailComposeDelegate = self
+
+            compseVC.setToRecipients(["carmu@email.com"])
+            compseVC.setSubject("Carmu 문의사항")
+            compseVC.setMessageBody("문의하실 내용을 작성해주세요.", isHTML: false)
+
+            self.present(compseVC, animated: true)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+
+    // 이메일 사용 불가 시 알럿
+    private func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(
+            title: "이메일 작성 실패",
+            message: "디바이스에 Mail 계정이 연결되어 있는지 확인하고 다시 시도해주세요.",
+            preferredStyle: .alert
+        )
+        let errorConfirm = UIAlertAction(title: "확인", style: .cancel)
+        sendMailErrorAlert.addAction(errorConfirm)
+        self.present(sendMailErrorAlert, animated: true)
+    }
 }
 
+// MARK: - 메일 보내기 관련 델리게이트 구현
+extension InquiryViewController: MFMailComposeViewControllerDelegate {
+    // 메일 보내기 버튼을 눌렀을 때 호출되는 델리게이트 메서드
+    func mailComposeController(
+        _ controller: MFMailComposeViewController,
+        didFinishWith result: MFMailComposeResult,
+        error: Error?
+    ) {
+        controller.dismiss(animated: true)
+    }
+}
+
+// MARK: - 테이블 뷰 관련 델리게이트 구현
 extension InquiryViewController: UITableViewDataSource, UITableViewDelegate {
 
     // 각 섹션의 row 수 반환
@@ -48,8 +91,7 @@ extension InquiryViewController: UITableViewDataSource, UITableViewDelegate {
             // TODO: - 평점 남기러 가기 구현하기
             print("평점 남기러 가기")
         } else {
-            // TODO: - 이메일로 문의하기 구현하기 (MFMailComposeViewController 사용)
-            print("이메일로 문의하기")
+            showEmailInquiryView()
         }
         // 클릭 후에는 셀의 선택이 해제된다.
         tableView.deselectRow(at: indexPath, animated: true)
