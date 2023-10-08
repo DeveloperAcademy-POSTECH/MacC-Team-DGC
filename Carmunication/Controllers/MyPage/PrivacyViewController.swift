ba3d0e8355203d4a19536d61d25410aa5c12204f
kybeen <rei980627@gmail.com>
2023-10-08 23:59:20 +0900

    [feat] #28 - 개인정보 처리방침 뷰 구성

--- Carmunication/Controllers/MyPage/PrivacyViewController.swift
+++ Carmunication/Controllers/MyPage/PrivacyViewController.swift
@@ -8,28 +8,17 @@
 import UIKit
 
 final class PrivacyViewController: UIViewController {
+
+    private let privacyView = PrivacyView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
 
         navigationItem.title = "개인정보 처리방침"
-    }
-}
-
-// MARK: - 프리뷰 canvas 세팅
-import SwiftUI
-
-struct PrivacyViewControllerRepresentable: UIViewControllerRepresentable {
-    typealias UIViewControllerType = PrivacyViewController
-    func makeUIViewController(context: Context) -> PrivacyViewController {
-        return PrivacyViewController()
-    }
-    func updateUIViewController(_ uiViewController: PrivacyViewController, context: Context) {
-    }
-}
-@available(iOS 13.0.0, *)
-struct PrivacyViewPreview: PreviewProvider {
-    static var previews: some View {
-        PrivacyViewControllerRepresentable()
+        view.addSubview(privacyView)
+        privacyView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide.snp.edges)
+        }
     }
 }
