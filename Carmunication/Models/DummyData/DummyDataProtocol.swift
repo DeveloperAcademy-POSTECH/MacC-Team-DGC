68d5124afacfcc272d58135301bdf2a5a78dc75f
dhk767 <dhk767@gmail.com>
2023-10-10 22:43:34 +0900

    [Feat] #38 - DummyData에 필요한 모델 생성, 문서 작업

---
+++ Carmunication/Models/DummyData/DummyDataProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  DummyData.swift
+//  Carmunication
+//
+//  Created by 김동현 on 2023/10/10.
+//
+
+import Foundation
+
+/**
+ 모든 더미데이터 모델 형식을 묶어주는 protocol
+ */
+protocol DummyData {
+    
+}
---
+++ Carmunication/Models/DummyData/Friendship.swift
@@ -0,0 +1,25 @@
+//
+//  Friendship.swift
+//  Carmunication
+//
+//  Created by 김동현 on 2023/10/10.
+//
+
+import Foundation
+
+/**
+ friend_id : Friendship 데이터 모델의 고유 번호
+ sender_id : 친구추가를 보낸 사람의 user_id
+ receiver_id : 친구추가를 받은 사람의 user_id
+ status : 친구 추가를 receiver가 받았는지 여부. 친구추가가 끊기게 되면, false로 변경
+ accumulate_distance_sender_stnd : sender가 캡틴인 기준으로 receiver와 함께 간 거리
+ accumulate_distance_receiver_stnd : receiver가 캡틴인 기준으로 sender와 함께 간 거리
+ */
+struct Friendship: DummyData {
+    var friend_id: Int
+    var sender_id: Int
+    var receiver_id: Int
+    var status: Bool = false
+    var accumulate_distance_sender_stnd: Int = 0
+    var accumulate_distance_receiver_stnd: Int = 0
+}
