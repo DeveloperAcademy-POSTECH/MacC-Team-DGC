67d7b6719b551c4d4b1b026d9d8f91f6705156b8
dhk767 <dhk767@gmail.com>
2023-10-12 16:14:20 +0900

    [Feat] #55 - 추소 설정 검색 테이블뷰 모달 컨트롤러, 뷰 복제 후 라벨 커스터마이징 완료

---
+++ Carmunication/Controllers/SessionList/SelectAddressViewController.swift
@@ -0,0 +1,113 @@
+//
+//  SelectPointMapViewController.swift
+//  Carmunication
+//
+//  Created by 김동현 on 2023/09/24.
+//
+
+import MapKit
+import UIKit
+
+import SnapKit
+
+final class SelectAddressViewController: UIViewController {
+
+    private let selectAddressView = SelectAddressView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(selectAddressView)
+        selectAddressView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        selectAddressView.closeButton.addTarget(
+            self,
+            action: #selector(closeFriendAddView),
+            for: .touchUpInside
+        )
+        selectAddressView.friendSearchButton.addTarget(
+            self,
+            action: #selector(performFriendSearch),
+            for: .touchUpInside
+        )
+        selectAddressView.clearButton.addTarget(
+            self,
+            action: #selector(clearButtonPressed),
+            for: .touchUpInside
+        )
+
+        selectAddressView.friendSearchTextField.delegate = self
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissTextField))
+        view.addGestureRecognizer(tapGesture)
+
+        // 키보드 노티피케이션 옵저버 등록
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+    }
+
+    // 클래스의 인스턴스가 메모리에서 해제되기 전에 호출되는 메서드
+    deinit {
+        // 옵저버 해제
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+}
+
+// MARK: - @objc 메서드
+extension SelectAddressViewController {
+
+    // 상단 닫기 버튼 클릭 시 동작
+    @objc private func closeFriendAddView() {
+        self.dismiss(animated: true)
+    }
+
+    // [검색] 버튼을 눌렀을 때 동작
+    @objc private func performFriendSearch() {
+        // TODO: - 친구 검색 기능 구현 필요
+        print("친구 검색")
+    }
+
+    // 텍스트필드 clear 버튼 눌렀을 때 동작
+    @objc private func clearButtonPressed() {
+        selectAddressView.friendSearchTextField.text = ""
+    }
+
+    // 텍스트 필드 비활성화 시 동작
+    @objc private func dismissTextField() {
+        selectAddressView.friendSearchTextFieldView.backgroundColor = .clear
+        selectAddressView.friendSearchTextFieldView.layer.borderWidth = 1.0
+        selectAddressView.friendSearchTextField.resignFirstResponder() // 최초 응답자 해제
+    }
+    
+    // 키보드가 나타날 때 호출되는 메서드
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+    }
+}
+
+// MARK: - UITextFieldDelegate 델리게이트 구현
+extension SelectAddressViewController: UITextFieldDelegate {
+
+    // 텍스트 필드의 편집이 시작될 때 호출되는 메서드
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        selectAddressView.friendSearchTextFieldView.backgroundColor = UIColor.semantic.backgroundSecond
+        selectAddressView.friendSearchTextFieldView.layer.borderWidth = 0
+    }
+
+    // 리턴 키를 눌렀을 때 호출되는 메서드
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dismissTextField()
+        return true
+    }
+}
