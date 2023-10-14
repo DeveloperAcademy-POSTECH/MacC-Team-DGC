188a6637e27a4e8efb20f463e5326a1c0bf7c115
dhk767 <dhk767@gmail.com>
2023-10-14 16:40:45 +0900

    [Chore] #61 - SelectDetailPointMapView 관련 View, VC 파일 생성, View/SessionList/GroupAdd 내부 Cell 뷰 폴더 이동

---
+++ Carmunication/Controllers/SessionList/GroupAdd/SelectDetailPointMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectDetailPointMapViewController.swift
+//  Carmunication
+//
+//  Created by 김동현 on 2023/10/14.
+//
+
+import UIKit
+
+class SelectDetailPointMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Carmunication/Views/SessionList/GroupAdd/SelectDetailPointMapView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectDetailPointMapView.swift
+//  Carmunication
+//
+//  Created by 김동현 on 2023/10/14.
+//
+
+import UIKit
+
+class SelectDetailPointMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
