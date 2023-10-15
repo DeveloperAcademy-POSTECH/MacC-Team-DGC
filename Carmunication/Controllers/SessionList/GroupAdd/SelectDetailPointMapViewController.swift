a2bfb5939adfe0d7214140e81369f9af3bc60115
dhk767 <dhk767@gmail.com>
2023-10-15 15:13:06 +0900

    [Feat] #60 - DetailPointMapView에서 지도 움직일 때, 상세 주소, 건물명 실시간으로 변동되는 기능 구현 완료

--- Carmunication/Controllers/SessionList/GroupAdd/SelectDetailPointMapViewController.swift
+++ Carmunication/Controllers/SessionList/GroupAdd/SelectDetailPointMapViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import NMapsMap
+
 class SelectDetailPointMapViewController: UIViewController {
 
     private let selectDetailPointMapView = SelectDetailPointMapView()
@@ -26,13 +28,84 @@ class SelectDetailPointMapViewController: UIViewController {
         view.backgroundColor = .white
         view.addSubview(selectDetailPointMapView)
 
+        selectDetailPointMapView.mapView.addCameraDelegate(delegate: self)
+
         selectDetailPointMapView.pointNameLabel.text = selectAddressModel.pointName
         selectDetailPointMapView.buildingNameLabel.text = selectAddressModel.buildingName
         selectDetailPointMapView.detailAddressLabel.text = selectAddressModel.detailAddress
         selectDetailPointMapView.centerMarkerLabel.text = selectAddressModel.pointName
 
+        selectDetailPointMapView.mapView.moveCamera(
+            NMFCameraUpdate(
+                scrollTo: NMGLatLng(
+                    lat: selectAddressModel.coordinate?.latitude ?? 36.66,
+                    lng: selectAddressModel.coordinate?.longitude ?? 128.33
+                )
+            )
+        )
         selectDetailPointMapView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Custom Method
+extension SelectDetailPointMapViewController {
+
+    private func getAddressAndBuildingName(
+        for coordinate: NMGLatLng,
+        completion: @escaping (String?, String?
+        ) -> Void) {
+        let geocoder = CLGeocoder()
+        let location = CLLocation(latitude: coordinate.lat, longitude: coordinate.lng)
+
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            guard error == nil else {
+                print("Error: \(error?.localizedDescription ?? "")")
+                completion(nil, nil)
+                return
+            }
+
+            if let placemark = placemarks?.first {
+                var placemarkDsc = placemark.description.components(separatedBy: ", ")
+                if placemarkDsc[0] == placemarkDsc[1] {
+                    placemarkDsc.remove(at: 0)
+                }
+                let buildingName = placemark.areasOfInterest?[0] ?? placemark.name ?? ""
+                var detailAddress = self.filterDetailAddress(placemarkDsc[1])
+
+                completion(buildingName, detailAddress)
+            } else {
+                completion(nil, nil)
+            }
+        }
+    }
+
+    // 상세주소 필터링
+    private func filterDetailAddress(_ words: String) -> String {
+        if let firstPart = words.split(separator: "@").first {
+            return String(firstPart).replacingOccurrences(of: "대한민국 ", with: "")
+        }
+        return ""
+    }
+}
+
+// MARK: - Naver Map Delegate Method
+extension SelectDetailPointMapViewController: NMFMapViewCameraDelegate {
+
+    func mapViewCameraIdle(_ mapView: NMFMapView) {
+        let center = mapView.projection.latlng(
+            from: CGPoint(
+                x: mapView.frame.size.width / 2,
+                y: mapView.frame.size.height / 2
+            )
+        )
+
+        // 주소와 건물명 가져오기
+        getAddressAndBuildingName(for: center) { buildingName, detailAddress in
+            // 주소와 건물명을 업데이트
+            self.selectDetailPointMapView.buildingNameLabel.text = buildingName
+            self.selectDetailPointMapView.detailAddressLabel.text = detailAddress
         }
     }
 }
