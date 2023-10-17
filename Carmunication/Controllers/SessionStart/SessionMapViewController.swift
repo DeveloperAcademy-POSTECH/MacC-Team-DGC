0b019e5e569b6a200dff257026eabbc621144fda
zhunhe <xuzhunhe@gmail.com>
2023-10-17 16:55:22 +0900

    [feat] #73 - 출발, 경유, 도착지 맵뷰에 표시

--- Carmunication/Controllers/SessionStart/SessionMapViewController.swift
+++ Carmunication/Controllers/SessionStart/SessionMapViewController.swift
@@ -11,6 +11,21 @@ import UIKit
 import NMapsMap
 import SnapKit
 
+// Firebase 데이터 받아오기 전까지만 사용하는 더미데이터
+struct Coordinate {
+    let lat: Double
+    let lng: Double
+}
+
+struct Points {
+    let startingPoint: Coordinate? = Coordinate(lat: 36.01759520, lng: 129.32206275)
+    let pickupLocation1: Coordinate? = Coordinate(lat: 36.00609523, lng: 129.32232291)
+    let pickupLocation2: Coordinate? = Coordinate(lat: 36.00739176, lng: 129.32907574)
+    let pickupLocation3: Coordinate? = nil
+    let destination: Coordinate? = Coordinate(lat: 36.0108783, lng: 129.327818)
+}
+// Firebase 데이터 받아오기 전까지만 사용하는 더미데이터
+
 final class SessionMapViewController: UIViewController {
 
     private let mapView = NMFMapView()
@@ -18,11 +33,40 @@ final class SessionMapViewController: UIViewController {
     private let carMarker = NMFMarker()
     private let locationManager = CLLocationManager()
 
+    private let startingPoint = {
+        let marker = NMFMarker()
+        return marker
+    }()
+
+    private let pickupLocation1 = {
+        let marker = NMFMarker()
+        marker.hidden = true
+        return marker
+    }()
+
+    private let pickupLocation2 = {
+        let marker = NMFMarker()
+        marker.hidden = true
+        return marker
+    }()
+
+    private let pickupLocation3 = {
+        let marker = NMFMarker()
+        marker.hidden = true
+        return marker
+    }()
+
+    private let destination = {
+        let marker = NMFMarker()
+        return marker
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         startUpdatingLocation()
         showNaverMap()
         showBackButton()
+        showPickuplocations()
     }
 
     private func showNaverMap() {
@@ -56,6 +100,44 @@ final class SessionMapViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         // 위도, 경도 정보 가져오기 시작
         locationManager.startUpdatingLocation()
+    }
+
+    // TODO: - 그룹의 실제 위경도 입력 받아서 넣어주기
+    private func showPickuplocations() {
+        let points = Points()
+
+        if let coordinate = points.startingPoint {
+            startingPoint.position = NMGLatLng(lat: coordinate.lat, lng: coordinate.lng)
+            startingPoint.iconImage = NMFOverlayImage(name: "startingPoint")
+            startingPoint.mapView = mapView
+        }
+
+        if let coordinate = points.pickupLocation1 {
+            pickupLocation1.position = NMGLatLng(lat: coordinate.lat, lng: coordinate.lng)
+            pickupLocation1.iconImage = NMFOverlayImage(name: "pickupLocation1")
+            pickupLocation1.hidden = false
+            pickupLocation1.mapView = mapView
+        }
+
+        if let coordinate = points.pickupLocation2 {
+            pickupLocation2.position = NMGLatLng(lat: coordinate.lat, lng: coordinate.lng)
+            pickupLocation2.iconImage = NMFOverlayImage(name: "pickupLocation2")
+            pickupLocation2.hidden = false
+            pickupLocation2.mapView = mapView
+        }
+
+        if let coordinate = points.pickupLocation3 {
+            pickupLocation3.position = NMGLatLng(lat: coordinate.lat, lng: coordinate.lng)
+            pickupLocation3.iconImage = NMFOverlayImage(name: "pickupLocation3")
+            pickupLocation3.hidden = false
+            pickupLocation3.mapView = mapView
+        }
+
+        if let coordinate = points.destination {
+            destination.position = NMGLatLng(lat: coordinate.lat, lng: coordinate.lng)
+            destination.iconImage = NMFOverlayImage(name: "destination")
+            destination.mapView = mapView
+        }
     }
 }
 
