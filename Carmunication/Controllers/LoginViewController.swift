e43815ec43b85096ac57895554d7b420bd9057e7
Taerogrammer <hyung54321@naver.com>
2023-10-18 10:40:43 +0900

    [chore] #79 - Line Length Violation 수정"

--- Carmunication/Controllers/LoginViewController.swift
+++ Carmunication/Controllers/LoginViewController.swift
@@ -137,8 +137,14 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
             return
         }
         print("FCMToken -> ", fcmToken)
-        let user = User(id: firebaseUser.uid, deviceToken: fcmToken, nickname: nickname, email: email, imageURL: "", friends: [])
-
+        let user = User(
+            id: firebaseUser.uid,
+            deviceToken: fcmToken,
+            nickname: nickname,
+            email: email,
+            imageURL: "",
+            friends: []
+        )
         do {
             let data = try encoder.encode(user)
 
