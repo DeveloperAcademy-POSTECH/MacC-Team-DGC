8db9f3f97b6e001ca83796416776f97208e183d3
Taerogrammer <hyung54321@naver.com>
2023-10-19 22:00:47 +0900

    [feat] #95 - 버튼 클릭 시 친구들에게 서버 푸시 보내기

--- Carmunication/Controllers/SessionStart/SessionStartViewController.swift
+++ Carmunication/Controllers/SessionStart/SessionStartViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+import Firebase
+import FirebaseDatabase
+import FirebaseFunctions
+import FirebaseMessaging
 import SnapKit
 
 final class SessionStartViewController: UIViewController {
@@ -14,6 +18,7 @@ final class SessionStartViewController: UIViewController {
     private let sessionStartView = SessionStartView()
     private let sessionStartMidView = SessionStartMidView()
     private let sessionStartMidNoGroupView = SessionStartMidNoGroupView()
+    var selectedGroupData: Group?
 
     // CaptainID
     private let captainID = "1"
@@ -21,12 +26,10 @@ final class SessionStartViewController: UIViewController {
     // 기기 크기에 따른 collectionView 높이 설정
     private var collectionViewHeight: CGFloat = 0
     private var collectionViewWidth: CGFloat = 0
-
     private var buttonHeight: CGFloat = 60
 
-    var selectedGroupData: Group?
-
     private var isInviteJourneyClicked = false
+    private var friendDeviceToken: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -116,6 +119,8 @@ extension SessionStartViewController {
             sessionStartView.journeyTogetherButton.backgroundColor = UIColor.semantic.negative
             sessionStartView.journeyTogetherButton.setTitle("바로 시작하기", for: .normal)
             isInviteJourneyClicked = true
+            // MARK: - 친구 목록에 있는 사람들에게 서버 푸시 알림 보내기
+            sendPush()
         }
     }
 
@@ -212,6 +217,114 @@ extension SessionStartViewController {
                 make.bottom.greaterThanOrEqualToSuperview().inset(tabBarControllerHeight + 20)
             }
             sessionStartView.journeyTogetherButton.layer.cornerRadius = buttonHeight / 2
+        }
+    }
+
+    private func sendPush() {
+        let functions = Functions.functions()
+        guard let databasePath = User.databasePathWithUID else {
+            return
+        }
+
+        // 유저의 친구 관계 리스트를 불러온다.
+        readUserFriendshipList(databasePath: databasePath) { friendshipList in
+            guard let friendshipList = friendshipList else {
+                return
+            }
+            // 친구 관계 id값으로 친구의 uid를 받아온다.
+            for friendshipID in friendshipList {
+                self.getFriendUid(friendshipID: friendshipID) { friendID in
+                    guard let friendID = friendID else {
+                        return
+                    }
+                    // 친구의 uid값으로 친구의 User객체를 불러온다.
+                    self.getFriendUser(friendID: friendID) { friend in
+                        guard let friend = friend else {
+                            return
+                        }
+                        self.friendDeviceToken.append(friend.deviceToken)
+                        print("친구들 Device Token : ", self.friendDeviceToken)
+
+                        // Functions 호출
+                        functions
+                            .httpsCallable("push_send")
+                            .call(["tokens": self.friendDeviceToken]) { (result, error) in
+                            if let error = error {
+                                print("Error calling Firebase Functions: \(error.localizedDescription)")
+                            } else {
+                                if let data = (result?.data as? [String: Any]) {
+                                    print("Response data -> ", data)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Firebase Realtime Database DB 관련 메서드
+extension SessionStartViewController {
+
+    // MARK: - DB에서 유저의 friendID 목록을 불러오는 메서드
+    private func readUserFriendshipList(databasePath: DatabaseReference, completion: @escaping ([String]?) -> Void) {
+        databasePath.child("friends").getData { error, snapshot in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            let friends = snapshot?.value as? [String]
+            completion(friends)
+        }
+    }
+    // MARK: - friendID 값으로 DB에서 Friendship의 친구 id를 불러오는 메서드
+    private func getFriendUid(friendshipID: String, completion: @escaping (String?) -> Void) {
+        Database.database().reference().child("friendship/\(friendshipID)").getData { error, snapshot in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            guard let snapshotValue = snapshot?.value as? [String: Any] else {
+                return
+            }
+            guard let currentUserID = KeychainItem.currentUserIdentifier else {
+                return
+            }
+            // sender와 receiver 중 현재 사용자에 해당하지 않는 uid를 뽑는다.
+            var friendID: String = ""
+            let senderValue = snapshotValue["senderID"] as? String ?? ""
+            let receiverValue = snapshotValue["receiverID"] as? String ?? ""
+            if currentUserID != senderValue {
+                friendID = senderValue
+            } else {
+                friendID = receiverValue
+            }
+            completion(friendID)
+        }
+    }
+    // MARK: - 친구의 uid로 DB에서 친구 데이터를 불러오기
+    private func getFriendUser(friendID: String, completion: @escaping (User?) -> Void) {
+        Database.database().reference().child("users/\(friendID)").getData { error, snapshot in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            guard let snapshotValue = snapshot?.value as? [String: Any] else {
+                return
+            }
+            let friend = User(
+                id: snapshotValue["id"] as? String ?? "",
+                deviceToken: snapshotValue["deviceToken"] as? String ?? "",
+                nickname: snapshotValue["nickname"] as? String ?? "",
+                email: snapshotValue["email"] as? String,
+                imageURL: snapshotValue["imageURL"] as? String,
+                friends: snapshotValue["friends"] as? [String]
+            )
+            completion(friend)
         }
     }
 }
