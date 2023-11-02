61395fca694034d62325cfc9c62005e5e80c5a3f
dhk767 <dhk767@gmail.com>
2023-11-02 19:07:30 +0900

    [Feat] #143 - 출발, 도착지 설정뷰 기본 파일 생성

---
+++ Carmu/Sources/Controllers/CrewMake/Driver/StartEndPointSelectViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartEndPointSelectViewController.swift
+//  Carmu
+//
+//  Created by 김동현 on 11/2/23.
+//
+
+import UIKit
+
+final class StartEndPointSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Carmu/Sources/Views/CrewMake/Driver/StartEndPointSelectView.swift
@@ -0,0 +1,32 @@
+//
+//  StartEndPointSelectView.swift
+//  Carmu
+//
+//  Created by 김동현 on 11/2/23.
+//
+
+import UIKit
+
+final class StartEndPointSelectView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func draw(_ rect: CGRect) {
+        setupViews()
+        setAutoLayout()
+    }
+
+    private func setupViews() {
+
+    }
+
+    private func setAutoLayout() {
+        
+    }
+}
