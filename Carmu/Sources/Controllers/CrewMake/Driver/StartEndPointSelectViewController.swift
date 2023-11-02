1696d7b2cb22fa72e7706713232070b4a2a4a64a
dhk767 <dhk767@gmail.com>
2023-11-02 19:42:03 +0900

    [Feat] #143 - StartEndPointSelectView 구현 완료

--- Carmu/Sources/Controllers/CrewMake/Driver/StartEndPointSelectViewController.swift
+++ Carmu/Sources/Controllers/CrewMake/Driver/StartEndPointSelectViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 final class StartEndPointSelectViewController: UIViewController {
 
+    private let startEndPointSelectView = StartEndPointSelectView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.semantic.backgroundDefault
 
+        view.addSubview(startEndPointSelectView)
+        startEndPointSelectView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
+}
+
+// MARK: - 프리뷰 canvas 세팅
+import SwiftUI
+
+struct SEPViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = StartEndPointSelectViewController
+    func makeUIViewController(context: Context) -> StartEndPointSelectViewController {
+        return StartEndPointSelectViewController()
+    }
+    func updateUIViewController(_ uiViewController: StartEndPointSelectViewController, context: Context) {}
 }
