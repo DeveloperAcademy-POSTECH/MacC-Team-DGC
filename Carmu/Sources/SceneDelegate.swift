6de10555fa46fb1b139dd5f5a3e561eebabdefb8
dhk767 <dhk767@gmail.com>
2023-11-03 16:47:36 +0900

    [Feat] #178 - isFisrt로만 rootViewController 설정 할 수 있도록 로직 변경

--- Carmu/Sources/SceneDelegate.swift
+++ Carmu/Sources/SceneDelegate.swift
@@ -13,11 +13,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     /** 
-     크루가 있는지 확인하는 임시변수. 추후 Firebase 데이터베이스 확인 후 설정하는 로직으로 변경예정
-     isFirst는 크루는 없지만, 건너뛰기를 탭한 사용자를 위한 변수
-     SessionStart로 시작하고 싶으면 hasCrew를 true로, isFirst를 false로 설정
+     초기진입인지 파악하는 변수. 추후 AppStorage로 관리 예저
+     크루가 없어도 SessionStart로 진입.
      */
-    static var hasCrew = false
     static var isFirst = true
 
     // 앱의 Scene이 처음으로 연결될 때 호출되는 메소드
@@ -38,15 +36,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // 로그인한 사용자가 있는지 체크
         if let currentUser = Auth.auth().currentUser {
-            switch (SceneDelegate.hasCrew, SceneDelegate.isFirst) {
-            case (true, _):
-                rootViewController = SessionStartViewController()
-            case (false, true):
+            if SceneDelegate.isFirst {
                 rootViewController = PositionSelectViewController()
-            case (false, false):
+            } else {
                 rootViewController = SessionStartViewController()
             }
-
             let navigationController = UINavigationController(rootViewController: rootViewController)
             window.rootViewController = navigationController
         } else {
