1fe80281598f01a8f85daabe26810fb2bcf5a1ff
dhk767 <dhk767@gmail.com>
2023-11-03 16:42:52 +0900

    [Feat] #178 - SceneDelegate 분기 설정 로직 정리

--- Carmu/Sources/SceneDelegate.swift
+++ Carmu/Sources/SceneDelegate.swift
@@ -12,8 +12,13 @@ import FirebaseAuth
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    // 크루가 있는지 확인하는 임시변수. 추후 Firebase 데이터베이스 확인 후 설정하는 로직으로 변경예정
+    /** 
+     크루가 있는지 확인하는 임시변수. 추후 Firebase 데이터베이스 확인 후 설정하는 로직으로 변경예정
+     isFirst는 크루는 없지만, 건너뛰기를 탭한 사용자를 위한 변수
+     SessionStart로 시작하고 싶으면 hasCrew를 true로, isFirst를 false로 설정
+     */
     static var hasCrew = false
+    static var isFirst = true
 
     // 앱의 Scene이 처음으로 연결될 때 호출되는 메소드
     func scene(
@@ -29,19 +34,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         self.window = window
 
-        // 로그인한 사용자가 있는지 체크
-        if Auth.auth().currentUser != nil {
+        var rootViewController: UIViewController
 
-            if SceneDelegate.hasCrew {
-                // MARK: - SessionStartViewController()
-                let rootViewController = SessionStartViewController()
-                let navigationController = UINavigationController(rootViewController: rootViewController)
-                window.rootViewController = navigationController
-            } else {
-                let rootViewController = PositionSelectViewController()
-                let navigationController = UINavigationController(rootViewController: rootViewController)
-                window.rootViewController = navigationController
+        // 로그인한 사용자가 있는지 체크
+        if let currentUser = Auth.auth().currentUser {
+            switch (SceneDelegate.hasCrew, SceneDelegate.isFirst) {
+            case (true, _):
+                rootViewController = SessionStartViewController()
+            case (false, true):
+                rootViewController = PositionSelectViewController()
+            case (false, false):
+                rootViewController = SessionStartViewController()
             }
+
+            let navigationController = UINavigationController(rootViewController: rootViewController)
+            window.rootViewController = navigationController
         } else {
             window.rootViewController = LoginViewController()
         }
