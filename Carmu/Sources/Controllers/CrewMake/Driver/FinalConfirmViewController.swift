f93bac34d75c898212b6b47396115c244a531d42
dhk767 <dhk767@gmail.com>
2023-11-03 10:34:39 +0900

    [Feat] #172 - 최종확인 기본 화면 구현

---
+++ Carmu/Sources/Controllers/CrewMake/Driver/FinalConfirmViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FinalConfirmViewController.swift
+//  Carmu
+//
+//  Created by 김동현 on 11/3/23.
+//
+
+import UIKit
+
+class FinalConfirmViewController: UIViewController {
+
+    private let finalConfirmView = FinalConfirmView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.semantic.backgroundDefault
+
+        view.addSubview(finalConfirmView)
+        finalConfirmView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - 프리뷰 canvas 세팅
+import SwiftUI
+
+struct FCViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = FinalConfirmViewController
+    func makeUIViewController(context: Context) -> FinalConfirmViewController {
+        return FinalConfirmViewController()
+    }
+    func updateUIViewController(_ uiViewController: FinalConfirmViewController, context: Context) {}
+}
