b8b1f955a5c61115071f82710a799886bf9a4296
Taerogrammer <hyung54321@naver.com>
2023-11-03 10:17:47 +0900

    [design] #168 -  운전자 앞면 화면 가운데 제외 디자인

--- Carmu/Sources/Views/SessionStart/SessionStartDriverView.swift
+++ Carmu/Sources/Views/SessionStart/SessionStartDriverView.swift
@@ -11,17 +11,175 @@ import SnapKit
 
 final class SessionStartDriverView: UIView {
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .red
+    private lazy var isFlipped = false
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.layer.masksToBounds = true
+        view.backgroundColor = .white
+        return view
+    }()
+
+    // 앞면 뷰
+    private lazy var driverFrontView = DriverFrontView()
+    // 뒷면 뷰
+    private lazy var driverBackView = DriverBackView()
+
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(flip))  // flip 메서드 만들기
+        addGestureRecognizer(tapGesture)
     }
 
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.frame = bounds
+        driverFrontView.frame = bounds
+        driverBackView.frame = bounds
+
     }
 }
 
-// MARK: - Data Methods
+// MARK: - Layout Methods
 extension SessionStartDriverView {
 
+    private func setupUI() {
+        addSubview(contentView)
+        contentView.addSubview(driverFrontView)
+        contentView.addSubview(driverBackView)
+        driverBackView.isHidden = true
+
+        layer.cornerRadius = 20
+        layer.shadowColor = UIColor.semantic.accPrimary?.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowRadius = 10
+    }
+}
+
+// MARK: - Actions
+extension SessionStartDriverView {
+
+    @objc private func flip() {
+        isFlipped.toggle()
+
+        let transitionOptions: UIView.AnimationOptions = isFlipped ? .transitionFlipFromLeft : .transitionFlipFromRight
+
+        UIView.transition(with: contentView, duration: 0.4, options: transitionOptions, animations: {
+            self.driverFrontView.isHidden = self.isFlipped
+            self.driverBackView.isHidden = !self.isFlipped
+        }, completion: nil)
+    }
+}
+
+// MARK: - 앞면 뷰
+final class DriverFrontView: UIView {
+    private lazy var comment: UILabel = {
+        let lbl = UILabel()
+
+        let firstLine = NSMutableAttributedString(string: "오늘 함께할 크루원들", attributes: [
+            .font: UIFont.carmuFont.headline1,
+            .foregroundColor: UIColor.semantic.textPrimary as Any
+        ])
+        let secondLine = NSMutableAttributedString(string: "\n\n실시간 탑승여부 응답 정보입니다", attributes: [
+            .font: UIFont.carmuFont.body2Long,
+            .foregroundColor: UIColor.semantic.textBody as Any
+        ])
+
+        firstLine.append(secondLine)
+
+        lbl.attributedText = firstLine
+        lbl.textAlignment = .left
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+
+    // /(총인원)에 대한 라벨
+    private lazy var totalCrewMemeberLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.carmuFont.subhead2
+        lbl.textColor = UIColor.semantic.textBody
+        return lbl
+    }()
+
+    // 실시간 탑승 여부 인원에 대한 라벨
+    lazy var todayCrewMemeberLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.carmuFont.headline1
+        lbl.textColor = UIColor.semantic.accPrimary
+        return lbl
+    }()
+
+    private lazy var crewView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.semantic.backgroundSecond
+        view.layer.cornerRadius = 16
+        return view
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        setupFrontView()
+        setupConstraints()
+        settingData()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupFrontView() {
+        addSubview(comment)
+        addSubview(totalCrewMemeberLabel)
+        addSubview(todayCrewMemeberLabel)
+        addSubview(crewView)
+    }
+
+    private func setupConstraints() {
+        comment.snp.makeConstraints { make in
+            make.leading.top.equalToSuperview().inset(20)
+        }
+        totalCrewMemeberLabel.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview().inset(20)
+        }
+        todayCrewMemeberLabel.snp.makeConstraints { make in
+            make.top.equalTo(totalCrewMemeberLabel)
+            make.centerY.equalTo(totalCrewMemeberLabel)
+            make.trailing.equalTo(totalCrewMemeberLabel.snp.leading).offset(-4)
+        }
+    }
+
+    // TODO: - 실제 데이터로 변경
+    private func settingData() {
+        totalCrewMemeberLabel.text = "/ \(groupData?.crews.count ?? 0)"
+        todayCrewMemeberLabel.text = "1"
+    }
+}
+
+// MARK: - 뒷면 뷰
+
+final class DriverBackView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        setupBackView()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupBackView() {
+
+    }
+
+    private func setupConstraints() {
+    }
 }
