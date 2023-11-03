54812bf08829d83cc1d05ad1706e8e71e7f0f766
dhk767 <dhk767@gmail.com>
2023-11-03 10:20:37 +0900

    [Feat] #172 - 시간 설정 기본 화면 구현

--- Carmu/Sources/SceneDelegate.swift
+++ Carmu/Sources/SceneDelegate.swift
@@ -31,7 +31,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if Auth.auth().currentUser != nil {
 
             // MARK: - SessionStartViewController()
-            let sessionStartViewController = SessionStartViewController()
+            let sessionStartViewController = TimeSelectViewController()
             let navigationController = UINavigationController(rootViewController: sessionStartViewController)
             window.rootViewController = navigationController
         } else {
