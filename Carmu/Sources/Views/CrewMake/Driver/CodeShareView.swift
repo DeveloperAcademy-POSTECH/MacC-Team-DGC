88e0ee50f8a0cf8cb9311458c54b5b38692c4977
dhk767 <dhk767@gmail.com>
2023-11-07 10:10:34 +0900

    [Feat] #156 - 코드 공유하기 뷰 구현 완료

--- Carmu/Sources/Views/CrewMake/Driver/CodeShareView.swift
+++ Carmu/Sources/Views/CrewMake/Driver/CodeShareView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import SnapKit
+
 final class CodeShareView: UIView {
 
     private lazy var secondLineTitleStack = UIStackView()
@@ -15,20 +17,44 @@ final class CodeShareView: UIView {
     private lazy var titleLabel2 = CrewMakeUtil.accPrimaryTitle(titleText: "초대코드를 공유")
     private lazy var titleLabel3 = CrewMakeUtil.defalutTitle(titleText: "해주세요")
 
-    lazy var nextButton: UIButton = {
+    private let characterImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "VariantGradientStroke")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let codeStackView: UIView = {
+        let stackView = UIView()
+        stackView.layer.borderWidth = 1
+        stackView.layer.borderColor = UIColor.theme.blue3?.cgColor
+        stackView.layer.cornerRadius = 20
+        stackView.clipsToBounds = true
+        return stackView
+    }()
+
+    var codeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.carmuFont.subhead3
+        label.textColor = UIColor.semantic.textPrimary
+        return label
+    }()
+
+    let copyButton: UIButton = {
         let button = UIButton()
-        button.setTitle("카풀 시작하기", for: .normal)
-        button.backgroundColor = UIColor.semantic.accPrimary
-        button.titleLabel?.font = UIFont.carmuFont.headline2
+        button.setTitle("복사 􀉁", for: .normal)
+        button.titleLabel?.font = UIFont.carmuFont.subhead2
         button.setTitleColor(UIColor.semantic.textSecondary, for: .normal)
+        button.backgroundColor = UIColor.semantic.accPrimary
         button.setBackgroundImage(
             UIImage(color: UIColor.semantic.textSecondary ?? .white),
             for: .highlighted
         )
-        button.layer.cornerRadius = 30
-
+        button.layer.cornerRadius = 20
         return button
     }()
+
+    let nextButton = NextButton(buttonTitle: "카풀 시작하기")
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -42,15 +68,23 @@ final class CodeShareView: UIView {
         setupViews()
         setAutoLayout()
     }
+}
 
+// MARK: - setup UI
+extension CodeShareView {
     private func setupViews() {
         secondLineTitleStack.axis = .horizontal
 
         secondLineTitleStack.addArrangedSubview(titleLabel2)
         secondLineTitleStack.addArrangedSubview(titleLabel3)
 
+        codeStackView.addSubview(codeLabel)
+        codeStackView.addSubview(copyButton)
+
         addSubview(titleLabel1)
         addSubview(secondLineTitleStack)
+        addSubview(characterImage)
+        addSubview(codeStackView)
         addSubview(nextButton)
     }
 
@@ -63,6 +97,29 @@ final class CodeShareView: UIView {
         secondLineTitleStack.snp.makeConstraints { make in
             make.top.equalTo(titleLabel1.snp.bottom)
             make.leading.equalToSuperview().inset(20)
+        }
+
+        characterImage.snp.makeConstraints { make in
+            make.top.equalTo(secondLineTitleStack.snp.bottom).offset(68)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(100)
+            make.height.equalTo(67)
+        }
+
+        codeStackView.snp.makeConstraints { make in
+            make.top.equalTo(characterImage.snp.bottom).offset(68)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.height.equalTo(36)
+        }
+
+        codeLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.centerY.equalToSuperview()
+        }
+
+        copyButton.snp.makeConstraints { make in
+            make.verticalEdges.trailing.equalToSuperview()
+            make.width.equalTo(80)
         }
 
         nextButton.snp.makeConstraints { make in
