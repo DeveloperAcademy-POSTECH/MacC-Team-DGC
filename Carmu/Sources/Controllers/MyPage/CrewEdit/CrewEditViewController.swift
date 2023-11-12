fd8d9ab30b5a5a482dd0d694c0a4734814eaf358
kybeen <rei980627@gmail.com>
2023-11-12 19:25:16 +0900

    [feat] #230 - 크루 편집 뷰, 뷰컨트롤러 생성

---
+++ Carmu/Sources/Controllers/MyPage/CrewEdit/CrewEditViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CrewEditViewController.swift
+//  Carmu
+//
+//  Created by 김영빈 on 2023/11/12.
+//
+
+import UIKit
+
+import SnapKit
+
+// MARK: - 크루 편집 뷰 컨트롤러
+final class CrewEditViewController: UIViewController {
+
+    private let crewEditView = CrewEditView()
+    private let firebaseManager = FirebaseManager()
+    var crewName: String
+
+    init(crewName: String) {
+        self.crewName = crewName
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.semantic.backgroundDefault
+
+        // 백버튼 텍스트 제거
+        navigationController?.navigationBar.topItem?.title = ""
+        // 설정 버튼 추가
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "완료",
+            style: .plain,
+            target: self,
+            action: #selector(completeCrewEdit)
+        )
+
+        view.addSubview(crewEditView)
+        crewEditView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        navigationItem.title = "크루명"
+        navigationItem.title = crewName
+    }
+}
+
+// MARK: - @objc 메서드
+extension CrewEditViewController {
+
+    // [완료] 버튼 클릭 시 호출
+    @objc private func completeCrewEdit() {
+        // TODO: - 구현하기
+        print("크루 편집 완료")
+    }
+}
---
+++ Carmu/Sources/Views/MyPage/CrewEdit/CrewEditView.swift
@@ -0,0 +1,79 @@
+//
+//  CrewEditView.swift
+//  Carmu
+//
+//  Created by 김영빈 on 2023/11/12.
+//
+
+import UIKit
+
+// MARK: - 마이페이지(운전자) 크루 정보 확인 뷰
+final class CrewEditView: UIView {
+
+    // 반복 요일 설정 버튼
+    private let repeatDayEditButton: UIButton = {
+        let repeatDayEditButton = UIButton()
+        // 폰트 및 텍스트 설정
+        let textFont = UIFont.carmuFont.subhead2
+        var titleAttr = AttributedString("  반복")
+        titleAttr.font = textFont
+        titleAttr.foregroundColor = UIColor.semantic.textBody
+        // SF Symbol 설정
+        let symbolFont = UIFont.boldSystemFont(ofSize: 20) // TODO: - 정확한 폰트 확인 필요
+        let symbolConfiguration = UIImage.SymbolConfiguration(font: symbolFont)
+        let symbolImage = UIImage(systemName: "calendar", withConfiguration: symbolConfiguration)
+
+        // 버튼 Configuration 설정
+        var config = UIButton.Configuration.filled()
+        config.attributedTitle = titleAttr
+        config.image = symbolImage
+        config.imagePlacement = .leading
+        config.background.cornerRadius = 17
+        config.baseBackgroundColor = UIColor.semantic.backgroundSecond
+        config.contentInsets = NSDirectionalEdgeInsets(
+            top: 5,
+            leading: 12,
+            bottom: 5,
+            trailing: 12
+        )
+        config.baseForegroundColor = UIColor.semantic.accPrimary
+        repeatDayEditButton.configuration = config
+        return repeatDayEditButton
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.semantic.backgroundDefault
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func setupUI() {
+        addSubview(repeatDayEditButton)
+        repeatDayEditButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(40)
+            make.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
+
+// MARK: - 프리뷰 canvas 세팅
+import SwiftUI
+
+struct CrewEditViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = CrewEditViewController
+    func makeUIViewController(context: Context) -> CrewEditViewController {
+        return CrewEditViewController(crewName: "크루명")
+    }
+    func updateUIViewController(_ uiViewController: CrewEditViewController, context: Context) {
+    }
+}
+@available(iOS 13.0.0, *)
+struct CrewEditViewPreview: PreviewProvider {
+    static var previews: some View {
+        CrewEditViewControllerRepresentable()
+    }
+}
