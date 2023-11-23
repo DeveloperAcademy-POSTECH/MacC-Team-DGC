e98c72da12b67a7d4e395bed33db0897eb19e91a
kybeen <rei980627@gmail.com>
2023-11-23 19:34:55 +0900

    [chore] #321 - 라이트모드 고정

--- Carmu/Sources/SceneDelegate.swift
+++ Carmu/Sources/SceneDelegate.swift
@@ -32,6 +32,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         let window = UIWindow(windowScene: windowScene)
         self.window = window
+        window.overrideUserInterfaceStyle = .light
 
         // 스플래시 뷰
         let navigationController = UINavigationController(rootViewController: LaunchScreenViewController())
