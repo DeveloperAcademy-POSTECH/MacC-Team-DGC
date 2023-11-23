dd4b89bfa4d4566242cc9629b480efbce30bdb58
zhunhe <xuzhunhe@gmail.com>
2023-11-23 14:53:18 +0900

    [refactor] #313 - 린트 경고 수정

--- Carmu/Sources/Views/SessionStart/SessionStartView.swift
+++ Carmu/Sources/Views/SessionStart/SessionStartView.swift
@@ -3,7 +3,8 @@ import UIKit
 final class SessionStartView: UIView {
 
     private let firebaseManger = FirebaseManager()
-    private let textColor = UIColor.semantic.accPrimary ?? .blue
+    private let titleLabelTintColor = UIColor.semantic.accPrimary ?? .blue
+    private let underLabelTintColor = UIColor.semantic.textTertiary ?? .blue
 
     lazy var myPageButton: UIButton = {
         let button = UIButton()
@@ -22,7 +23,7 @@ final class SessionStartView: UIView {
         return label
     }()
 
-    lazy var notifyComment: UILabel = {
+    let underLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
@@ -79,8 +80,8 @@ final class SessionStartView: UIView {
             make.top.equalTo(myPageButton.snp.bottom)
         }
 
-        addSubview(notifyComment)
-        notifyComment.snp.makeConstraints { make in
+        addSubview(underLabel)
+        underLabel.snp.makeConstraints { make in
             make.bottom.equalToSuperview().inset(142.5)
             make.horizontalEdges.equalToSuperview().inset(20)
         }
@@ -131,7 +132,7 @@ extension SessionStartView {
 
     private func setTitleLabelNoCrew() {
         titleLabel.text = "오늘도 카뮤와 함께\n즐거운 하루되세요!"
-        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: ["카뮤"], color: textColor)
+        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: ["카뮤"], color: titleLabelTintColor)
     }
 
     private func setTitleForDriver(crewData: Crew) {
@@ -175,36 +176,36 @@ extension SessionStartView {
     private func setTitleDriverWaiting(crewData: Crew) {
         let crewName = crewData.name ?? ""
         titleLabel.text = "\(crewName),\n오늘 운행하시나요?"
-        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: textColor)
+        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: titleLabelTintColor)
     }
 
     private func setTitleDriverAccept(crewData: Crew) {
         if firebaseManger.isAnyMemberAccepted(crewData: crewData) {
             let crewName = crewData.name ?? ""
             titleLabel.text = "\(crewName),\n운행을 시작해볼까요?"
-            titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: textColor)
+            titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: titleLabelTintColor)
         } else {
             titleLabel.text = "탑승자들의\n응답을 기다립니다"
-            titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: ["탑승자들", "응답"], color: textColor)
+            titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: ["탑승자들", "응답"], color: titleLabelTintColor)
         }
     }
 
     private func setTitleDriverSessionStart(crewData: Crew) {
         let crewName = crewData.name ?? ""
         titleLabel.text = "\(crewName),\n안전 운행하세요"
-        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: textColor)
+        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: titleLabelTintColor)
     }
 
     private func setTitleMemberSessionStart(crewData: Crew) {
         let crewName = crewData.name ?? ""
         titleLabel.text = "\(crewName)이\n시작되었습니다!"
-        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: textColor)
+        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: titleLabelTintColor)
     }
 
     private func setTitleMemberDefault(crewData: Crew) {
         let crewName = crewData.name ?? ""
         titleLabel.text = "\(crewName)과\n함께 가시나요?"
-        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: textColor)
+        titleLabel.attributedText = setColorToLabel(text: titleLabel.text, coloredTexts: [crewName], color: titleLabelTintColor)
     }
 
     private func setColorToLabel(text: String?, coloredTexts: [String], color: UIColor) -> NSMutableAttributedString {
@@ -237,7 +238,7 @@ extension SessionStartView {
     }
 
     private func setUnderLabelNoCrew() {
-        notifyComment.text = ""
+        underLabel.text = ""
     }
 
     private func setUnderForDriver(crewData: Crew) {
@@ -269,45 +270,45 @@ extension SessionStartView {
 
     // MARK: - Driver 상황별 텍스트 변경 메서드
     private func setUnderDriverWaiting() {
-        notifyComment.text = "오늘의 셔틀 운행 여부를\n출발시간 30분 전까지 알려주세요!"
-        notifyComment.attributedText = setColorToLabel(text: notifyComment.text, coloredTexts: ["30분 전"], color: UIColor.semantic.textTertiary ?? .blue, lineHeight: 8)
+        underLabel.text = "오늘의 셔틀 운행 여부를\n출발시간 30분 전까지 알려주세요!"
+        underLabel.attributedText = setColorToLabel(text: underLabel.text, coloredTexts: ["30분 전"], color: underLabelTintColor, lineHeight: 8)
     }
 
     private func setUnderDriverDecline(crewData: Crew) {
-        notifyComment.text = "오늘의 카풀 운행 여부를\n전달했어요"
+        underLabel.text = "오늘의 카풀 운행 여부를\n전달했어요"
     }
 
     private func setUnderDriverAccept(crewData: Crew) {
         if firebaseManger.isAnyMemberAccepted(crewData: crewData) {
-            notifyComment.text = "현재 탑승 응답한 탑승자들과 셔틀을 시작할까요?"
+            underLabel.text = "현재 탑승 응답한 탑승자들과 셔틀을 시작할까요?"
         } else {
-            notifyComment.text = "오늘의 셔틀 운행 여부를 전달했어요\n탑승 응답을 확인중입니다..."
+            underLabel.text = "오늘의 셔틀 운행 여부를 전달했어요\n탑승 응답을 확인중입니다..."
         }
     }
 
     private func setUnderDriverSessionStart(crewData: Crew) {
-        notifyComment.text = "현재 운행 중인 셔틀이 있습니다\n셔틀 지도보기를 눌러주세요!"
-        notifyComment.attributedText = setColorToLabel(text: notifyComment.text, coloredTexts: ["현재 운행중인 셔틀", "셔틀 지도보기"], color: UIColor.semantic.textTertiary ?? .blue, lineHeight: 8)
+        underLabel.text = "현재 운행 중인 셔틀이 있습니다\n셔틀 지도보기를 눌러주세요!"
+        underLabel.attributedText = setColorToLabel(text: underLabel.text, coloredTexts: ["현재 운행중인 셔틀", "셔틀 지도보기"], color: underLabelTintColor, lineHeight: 8)
     }
 
     // MARK: - Member 상황별 텍스트 변경 메서드
     private func setUnderMemberPassengerStatusDecline() {
-        notifyComment.text = ""
+        underLabel.text = ""
     }
 
     private func setUnderMemberSessionStart(crewData: Crew) {
         let crewName = crewData.name ?? ""
-        notifyComment.text = "\(crewName)이\n시작되었습니다!"
-        notifyComment.attributedText = setColorToLabel(text: notifyComment.text, coloredTexts: [crewName], color: UIColor.semantic.textTertiary ?? .blue, lineHeight: 8)
+        underLabel.text = "\(crewName)이\n시작되었습니다!"
+        underLabel.attributedText = setColorToLabel(text: underLabel.text, coloredTexts: [crewName], color: underLabelTintColor, lineHeight: 8)
     }
 
     private func setUnderMemberDefault(crewData: Crew) {
-        notifyComment.text = "오늘의 셔틀 참여여부를\n탑승시간 20분 전까지 알려주세요!"
-        notifyComment.attributedText = setColorToLabel(text: notifyComment.text, coloredTexts: ["20분 전"], color: UIColor.semantic.textTertiary ?? .blue, lineHeight: 8)
+        underLabel.text = "오늘의 셔틀 참여여부를\n탑승시간 20분 전까지 알려주세요!"
+        underLabel.attributedText = setColorToLabel(text: underLabel.text, coloredTexts: ["20분 전"], color: underLabelTintColor, lineHeight: 8)
     }
 
     private func setUnderMemberDecline() {
-        notifyComment.text = "기사님의 사정으로\n오늘은 셔틀이 운행되지 않아요"
+        underLabel.text = "기사님의 사정으로\n오늘은 셔틀이 운행되지 않아요"
     }
 
     private func setColorToLabel(text: String?, coloredTexts: [String], color: UIColor, lineHeight: CGFloat) -> NSMutableAttributedString {
