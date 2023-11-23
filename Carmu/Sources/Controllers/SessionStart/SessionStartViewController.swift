e8b9ac4ed21d309360d2748a524f441d6e69061f
zhunhe <xuzhunhe@gmail.com>
2023-11-23 16:20:34 +0900

    [refactor] #313 - SessionStartViewController 코드 정리중

--- Carmu/Sources/Controllers/SessionStart/SessionStartViewController.swift
+++ Carmu/Sources/Controllers/SessionStart/SessionStartViewController.swift
@@ -100,88 +100,16 @@ extension SessionStartViewController {
 
     // UI를 업데이트 시켜줌
     private func showCrewCardView(crewData: Crew) {
-        if isCaptain {
-            settingDriverData(crewData: crewData)
-        } else {
-            settingPassengerData(crewData: crewData)
-        }
-
-        checkingCrewStatus(crewData: crewData)
-
-        if isCaptain {
-            driverCardView.driverFrontView.crewData = crewData
-            driverCardView.driverFrontView.settingDriverFrontData(crewData: crewData)
-            driverCardView.driverFrontView.crewCollectionView.reloadData()
-        } else {
-            memberCardView.passengerFrontView.settingPassengerFrontData(crewData: crewData)
-
-            if crewData.sessionStatus == .accept {  // 운전자가 운행할 때
-                memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "DriverBlinker")
-                memberCardView.passengerFrontView.statusLabel.text = "오늘은 카풀이 운행될 예정이에요"
-            }
-
-            // 탑승자의 참석 여부에 따른 분기문
-            if firebaseManager.passengerStatus(crewData: crewData) == .accept {
-            } else if firebaseManager.passengerStatus(crewData: crewData) == .decline {
-                switch crewData.sessionStatus {
-                case .waiting:
-                    break
-                case .accept:
-                    memberCardView.passengerFrontView.noDriveComment.text = "오늘은 카풀에 참여하지 않으시군요!\n내일 봐요!"
-                    memberCardView.passengerFrontView.noDriveComment.textColor = UIColor.semantic.textPrimary
-                    memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = false
-                case .decline: break
-                case .sessionStart: break
-                    // sessionStart일 때는 해당 버튼이 나타나지 않음
-                case .none: break
-                }
-            }
-        }
-
         noCrewCardView.isHidden = true
+
         if isCaptain {
-            settingDriverView(crewData: crewData)
+            showDriverCardView(crewData: crewData)
         } else {
-            settingPassengerView(crewData: crewData)
+            showMemberCardView(crewData: crewData)
         }
     }
-}
 
-// MARK: - 크루가 없을 때
-extension SessionStartViewController {
-
-    private func showNoCrewCardView() {
-        driverCardView.isHidden = true
-        memberCardView.isHidden = true
-        noCrewCardView.isHidden = false
-
-        noCrewCardView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(20)
-            make.top.equalTo(backgroundView.titleLabel.snp.bottom).offset(36)
-            make.bottom.lessThanOrEqualToSuperview().inset(165)
-            make.bottom.equalToSuperview().inset(165)
-        }
-    }
-}
-
-// MARK: - 크루가 있을 때 - 공통
-/**
- 경우의 수가 매우 많아 헷갈리지 않기 위해 모든 메서드를 다음과 같이 분리하겠습니다.
-    if 운전자인 경우
-        - waiting
-        - accept
-        - decline
-        - sessionStart
-    else 동승자인 경우
-        - waiting
-        - accept
-        - decline
-        - sessionStart
- */
-extension SessionStartViewController {
-
-    // 운전자일 때
-    private func settingDriverData(crewData: Crew) {
+    private func showDriverCardView(crewData: Crew) {
         switch crewData.sessionStatus {
         case .waiting:
             driverCardView.driverFrontView.noDriveViewForDriver.isHidden = true
@@ -189,55 +117,16 @@ extension SessionStartViewController {
             driverCardView.driverFrontView.noDriveViewForDriver.isHidden = true
         case .decline:
             driverCardView.driverFrontView.noDriveViewForDriver.isHidden = false
-            settingDataDecline()
+            driverCardView.layer.opacity = 1.0
         case .sessionStart:
-            settingDataSessionStart(crewData: crewData)
+            driverCardView.layer.opacity = 1.0
         case .none: break
         }
-    }
 
-    // 동승자일 때
-    private func settingPassengerData(crewData: Crew) {
-        switch crewData.sessionStatus {
-        case .waiting:
-            memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = true
-            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "HourglassBlinker")
-            memberCardView.passengerFrontView.statusLabel.text = "운전자의 확인을 기다리고 있어요"
-        case .accept:
-            // 당일 운행이 없다는 뷰 숨기기
-            memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = true
-            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "DriverBlinker")
-            memberCardView.passengerFrontView.statusLabel.text = "오늘은 카풀이 운행될 예정이에요"
-        case .decline:
-            memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = false
-            memberCardView.passengerFrontView.noDriveComment.text = "오늘은 카풀이 운행되지 않아요"
-            memberCardView.passengerFrontView.noDriveComment.textColor = UIColor.semantic.negative
-            settingDataDecline()
-        case .sessionStart:
-            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "DriverBlinker")
-            memberCardView.passengerFrontView.statusLabel.text = "오늘은 카풀이 운행될 예정이에요"
-            settingDataSessionStart(crewData: crewData)
-        case .none: break
-        }
-    }
+        driverCardView.driverFrontView.crewData = crewData
+        driverCardView.driverFrontView.settingDriverFrontData(crewData: crewData)
+        driverCardView.driverFrontView.crewCollectionView.reloadData()
 
-    // 공통(운전자, 탑승자)으로 사용되는 메서드
-    // settingData - .decline
-    private func settingDataDecline() {
-        driverCardView.layer.opacity = 1.0
-    }
-
-    // settingData - .sessionStart
-    private func settingDataSessionStart(crewData: Crew) {
-        driverCardView.layer.opacity = 1.0
-    }
-}
-
-// MARK: - 크루가 있을 때 - 운전자일 때
-extension SessionStartViewController {
-
-    // MARK: - ViewWillAppear에서 하면 될 듯,,?
-    private func settingDriverView(crewData: Crew) {
         // 비활성화
         driverCardView.layer.opacity = 0.5
 
@@ -252,15 +141,52 @@ extension SessionStartViewController {
         }
     }
 
-    func showShuttleFinishedModal() {
-        present(SessionFinishViewController(), animated: true)
-    }
-}
+    private func showMemberCardView(crewData: Crew) {
+        switch crewData.sessionStatus {
+        case .waiting:
+            memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = true
+            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "HourglassBlinker")
+            memberCardView.passengerFrontView.statusLabel.text = "운전자의 확인을 기다리고 있어요"
+        case .accept:
+            // 당일 운행이 없다는 뷰 숨기기
+            memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = true
+            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "DriverBlinker")
+            memberCardView.passengerFrontView.statusLabel.text = "오늘은 카풀이 운행될 예정이에요"
+        case .decline:
+            memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = false
+            memberCardView.passengerFrontView.noDriveComment.text = "오늘은 카풀이 운행되지 않아요"
+            memberCardView.passengerFrontView.noDriveComment.textColor = UIColor.semantic.negative
+            driverCardView.layer.opacity = 1.0
+        case .sessionStart:
+            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "DriverBlinker")
+            memberCardView.passengerFrontView.statusLabel.text = "오늘은 카풀이 운행될 예정이에요"
+            driverCardView.layer.opacity = 1.0
+        case .none: break
+        }
 
-// MARK: - 크루가 있을 때 - 크루원일 때
-extension SessionStartViewController {
+        memberCardView.passengerFrontView.settingPassengerFrontData(crewData: crewData)
 
-    private func settingPassengerView(crewData: Crew) {
+        if crewData.sessionStatus == .accept {  // 운전자가 운행할 때
+            memberCardView.passengerFrontView.statusImageView.image = UIImage(named: "DriverBlinker")
+            memberCardView.passengerFrontView.statusLabel.text = "오늘은 카풀이 운행될 예정이에요"
+        }
+
+        // 탑승자의 참석 여부에 따른 분기문
+        if firebaseManager.passengerStatus(crewData: crewData) == .decline {
+            switch crewData.sessionStatus {
+            case .waiting:
+                break
+            case .accept:
+                memberCardView.passengerFrontView.noDriveComment.text = "오늘은 카풀에 참여하지 않으시군요!\n내일 봐요!"
+                memberCardView.passengerFrontView.noDriveComment.textColor = UIColor.semantic.textPrimary
+                memberCardView.passengerFrontView.noDriveViewForPassenger.isHidden = false
+            case .decline: break
+            case .sessionStart: break
+                // sessionStart일 때는 해당 버튼이 나타나지 않음
+            case .none: break
+            }
+        }
+
         // view layout
         driverCardView.isHidden = true
         memberCardView.isHidden = false
@@ -269,6 +195,19 @@ extension SessionStartViewController {
             make.leading.trailing.equalToSuperview().inset(20)
             make.top.lessThanOrEqualTo(backgroundView.myPageButton.snp.bottom).offset(88)
             make.bottom.lessThanOrEqualToSuperview().inset(216)
+        }
+    }
+
+    private func showNoCrewCardView() {
+        driverCardView.isHidden = true
+        memberCardView.isHidden = true
+        noCrewCardView.isHidden = false
+
+        noCrewCardView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(backgroundView.titleLabel.snp.bottom).offset(36)
+            make.bottom.lessThanOrEqualToSuperview().inset(165)
+            make.bottom.equalToSuperview().inset(165)
         }
     }
 }
@@ -286,7 +225,6 @@ extension SessionStartViewController {
         if isCaptain {
             firebaseManager.driverTogetherButtonTapped(crewData: crewData)
 
-            // 활성화
             driverCardView.layer.opacity = 1.0
         } else {
             firebaseManager.passengerTogetherButtonTapped(crewData: crewData)
@@ -311,7 +249,11 @@ extension SessionStartViewController {
     @objc private func individualButtonDidTapped() {
         guard let crewData = crewData else { return }
         if isCaptain {
-            settingIndividualButtonForDriver(crewData: crewData)
+            firebaseManager.driverIndividualButtonTapped(crewData: crewData)
+
+            driverCardView.driverFrontView.noDriveViewForDriver.isHidden = false
+            driverCardView.driverFrontView.crewCollectionView.isHidden = true   // 컬렉션뷰 가리고 오늘 가지 않는다는 뷰 보여주기
+            driverCardView.layer.opacity = 1.0
         } else {
             firebaseManager.passengerIndividualButtonTapped(crewData: crewData)
         }
@@ -326,27 +268,11 @@ extension SessionStartViewController {
     }
 }
 
-// MARK: - individualButton Methods
-extension SessionStartViewController {
-
-    // 운전자일 때
-    private func settingIndividualButtonForDriver(crewData: Crew) {
-        firebaseManager.driverIndividualButtonTapped(crewData: crewData)
-
-        // 모든 경우에 같은 화면임
-        driverCardView.driverFrontView.noDriveViewForDriver.isHidden = false
-        driverCardView.driverFrontView.crewCollectionView.isHidden = true   // 컬렉션뷰 가리고 오늘 가지 않는다는 뷰 보여주기
-        driverCardView.layer.opacity = 1.0
-    }
-}
-
 // MARK: - Action
 extension SessionStartViewController {
 
-    // 함께하는 크루원이 한 명 이상일 때 버튼 Enable
-    private func checkingCrewStatus(crewData: Crew) {
-        if firebaseManager.isAnyMemberAccepted(crewData: crewData) {
-        }
+    func showShuttleFinishedModal() {
+        present(SessionFinishViewController(), animated: true)
     }
 
     // 가이드 화면 보여주는 메서드
@@ -367,9 +293,6 @@ extension SessionStartViewController {
         let now = Date().toString24HourClock.toMinutes
         return now < startTime && now > arrivalTime + crew.lateTime + 10
     }
-}
-
-extension SessionStartViewController {
 
     // 디바이스 토큰값이 업데이트가 될 수도 있기 때문에 업데이트된 값을 저장하기 위한 메서드
     private func settingDeviceToken() {
